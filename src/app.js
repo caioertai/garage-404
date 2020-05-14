12c567e6b02c17003b8d96ca6d5753437b2b6f00
Caio Andrade <caioertai@gmail.com>
2020-05-14 17:31:35 -0500

    Livecode comments

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 // You can check the documentation Postman automatically creates in here:
 // https://documenter.getpostman.com/view/1672725/SzmiXcDN?version=latest
 
-// Importing stuff
+// ============================================================================
+// Import libraries and plugins
+// ============================================================================
 import { fetchCars, handleCarForm } from "./cars";
 
-// Ajax Call
+// ============================================================================
+// Make ajax calls
+// ============================================================================
 fetchCars();
 
+// ============================================================================
 // Assign behaviors
+// ============================================================================
 const carForm = document.querySelector("#new-car");
 carForm.addEventListener("submit", handleCarForm);
--- src/cars.js
+++ src/cars.js
@@ -1,60 +1,82 @@
-const baseURL = "https://wagon-garage-api.herokuapp.com";
+// ============================================================================
+// Parameters
+// ============================================================================
+// Base URL of our API (with the wat-garage :slug)
+// Check the documentation on the livecode description for more
+const baseURL = "https://wagon-garage-api.herokuapp.com/wat-garage";
 
-// Select Elements
+// ============================================================================
+// Select Elements (check index.html to understand them)
+// ============================================================================
+// The list where the cars will be inserted
 const carsList = document.querySelector(".cars-list");
+
+// The inputs from the car form
 const brandInput = document.querySelector("#brand");
 const modelInput = document.querySelector("#model");
 const plateInput = document.querySelector("#plate");
 const ownerInput = document.querySelector("#owner");
 
+// ============================================================================
+// Functions
+// ============================================================================
+// Build a car tag when given a car object like { brand: "", model: "", ... }
+// as does our API
 const carTagBuilder = (car) => {
-  // return a car tag
+  const carName = `${car.brand} ${car.model}`;
   return `
     <div class="car">
       <div class="car-image">
-        <img src="http://loremflickr.com/280/280/${car.brand} ${car.model}" />
+        <img src="http://loremflickr.com/280/280/${carName}" />
       </div>
       <div class="car-info">
-        <h4>${car.brand} ${car.model}</h4>
+        <h4>${carName}</h4>
         <p><strong>Owner:</strong> ${car.owner}</p>
         <p><strong>Plate:</strong> ${car.plate}</p>
       </div>
     </div>
-  `
+  `;
 };
 
+// Inserts a car into the list when give a car object
 const insertCar = (car) => {
+  // Arguments:                  where         HTML string
   carsList.insertAdjacentHTML("beforeEnd", carTagBuilder(car));
 };
 
+// Displays cars in the HTML When given an array of car objects
 const displayCars = (cars) => {
+  // Clean the list
   carsList.innerHTML = "";
+  // Start inserting them with the function insertCat above
   cars.forEach(insertCar);
 };
 
+// Gets cars from the API
+// GET /cars
 const fetchCars = () => {
-  fetch(`${baseURL}/wat-garage/cars`)
+  fetch(`${baseURL}/cars`)
     .then(response => response.json())
     .then(displayCars);
 };
 
+// Posts cars in the API when given a car object
+// POST /cars
 const postCar = (carObject) => {
-  // Post car
-  // Build the fetch POST with the info
-  fetch(`${baseURL}/wat-garage/cars`, {
+  fetch(`${baseURL}/cars`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(carObject)
   }).then(response => response.json())
-    .then((data) => { fetchCars(); })
-
+    .then(fetchCars);
 };
 
+// Defines the behavior of the car form
 const handleCarForm = (event) => {
+  // Stop form from doing its default (requesting a page)
   event.preventDefault();
 
-  // Get the form input values
-  // Build a car object
+  // Get the form input values to build a car object
   const car = {
     brand: brandInput.value,
     model: modelInput.value,
@@ -62,7 +84,12 @@ const handleCarForm = (event) => {
     plate: ownerInput.value
   };
 
+  // Post the car to the API
   postCar(car);
 };
 
+// ============================================================================
+// Export
+// ============================================================================
+// Make fetchCars and handleCarForm available for import in app.js
 export { fetchCars, handleCarForm };
